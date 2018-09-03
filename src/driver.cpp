93ab5ecec8d89bee52d349ee66d3c48be6ea5158
xkl <1125290220@qq.com>
2018-09-03 16:52:21 +0800

    ec_txPDO

--- src/driver.cpp
+++ src/driver.cpp
@@ -10,6 +10,8 @@
 #include <soem/ethercatfoe.h>
 #include <soem/ethercatconfig.h>
 #include <soem/ethercatprint.h>
+#include<stdio.h>
+#include<string.h>
 
 #define IFRAME "eth1"
 
@@ -18,8 +20,20 @@ using namespace std;
 int main(){
 
 	int ret;
-	ret = ec_init("eth0");			// 初始化soem
+	char iframe[1024];
+	string siframe = "eth0";
+	float buffer[6];
+
+	int psize = sizeof(buffer);
+
+	cout << "sizeof(buffer) = " << sizeof(buffer) << endl;
+
+	strncpy(iframe, siframe.c_str(), 1024);
+
+	ret = ec_init(iframe);			// 初始化soem
 	cout << "ret = " << ret << endl; 
+    perror("--------------");
+
 	if(ret < 0){
 		cout << "ec_init error, ret = " << ret << endl;
 		return -1;
@@ -33,7 +47,10 @@ int main(){
 
 	cout << "ec_slavecount = " << ec_slavecount << endl;
 	if(ec_slavecount > 0)
-		cout << "ec_slave0 name was: " << ec_slave[0].name << endl;
+		cout << "ec_slave0 name was: " << ec_slave[1].name << endl;
+
+	ec_TxPDO(1, 0x6000, &psize, buffer, 100);
+	cout << "psize = " << psize << endl;
 
 	sleep(10);
 	return 0;
